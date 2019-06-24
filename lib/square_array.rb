5c9d3de18c32904bfd4a53073c8e8657460c80ee
Doug Kintop <dkintop@hotmail.com>
2019-06-24 22:27:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -10,6 +10,40 @@ def square_array(array)
  
 new_array= []
 new_array << result
+
+counter += 1
+end
+
+new_array
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def square_array(array)
+ counter = 0
+ 
+ 
+ while array[counter] do
+ result= array[counter] **2
+
+ 
+new_array= []
+new_array << result
 puts new_array
 
 counter += 1
@@ -19,14 +53,7 @@ new_array
 
 end
 
-#the above method will square a value at a designated index of an array. 
-#next challenge is to use a while loop to push the new value to  new array.
-
-
-
-
-
-
+#the above example puts the results to the terminal, but only returns the last value. closest you have gotten so far.
 
 
 
