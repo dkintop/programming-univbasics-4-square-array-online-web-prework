648dbbbaaf6a1989321ecf4f56f7d2e5d4b2803b
Doug Kintop <dkintop@hotmail.com>
2019-06-24 22:38:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -34,24 +34,6 @@ end
 
 
 
-def square_array(array)
- counter = 0
- 
- 
- while array[counter] do
- result= array[counter] **2
-
- 
-new_array= []
-new_array << result
-puts new_array
-
-counter += 1
-end
-
-new_array
-
-end
 
 #the above example puts the results to the terminal, but only returns the last value. closest you have gotten so far.
 
