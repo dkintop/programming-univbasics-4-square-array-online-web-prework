1b7559bf58dcce7cb6c354f6a2c5f0fc4584c79d
Doug Kintop <dkintop@hotmail.com>
2019-06-24 22:40:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,7 +9,7 @@ def square_array(array)
 
  
 new_array= []
-new_array.push (result)
+new_array[counter] = result
 puts new_array
 counter += 1
 end
@@ -34,7 +34,23 @@ end
 
 
 
+def square_array(array)
+ counter = 0
+ 
+ 
+ while array[counter] do
+ result= array[counter] **2
 
+ 
+new_array= []
+new_array.push (result)
+puts new_array
+counter += 1
+end
+
+new_array
+
+end
 #the above example puts the results to the terminal, but only returns the last value. closest you have gotten so far.
 
 
